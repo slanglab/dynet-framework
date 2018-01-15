added softmax builder and fixed sampling

lm.py
@@ -2,10 +2,8 @@ import numpy as np
 import _dynet as dy
 
 class LanguageModelBase:
-    def to_sequence_batch(self, decoding, out_vocab):
-        batch_size = len(decoding[0])
-        decoding = [  [ x[i] for x in decoding ] for i in range(0, batch_size) ]
-        decoding = [ [ out_vocab[y] for y in x ] for x in decoding ]
+    def to_sequence(self, decoding, out_vocab):
+        decoding = [ out_vocab[x] for x in decoding ]
         return decoding
 
     def one_batch(self, X_batch, y_batch, X_masks, y_masks, eos=133, training=True):
@@ -24,7 +22,7 @@ class LanguageModelBase:
         for x, y, mask in zip(decoding, y_batch, y_masks):
             mask_expr = dy.inputVector(mask)
             mask = dy.reshape(mask_expr, (1,), batch_size)
-            batch_loss.append(mask * dy.pickneglogsoftmax_batch(x, y))
+            batch_loss.append(mask * self.pickneglogsoftmax_batch(x, y))    #calls subclass function...
         batch_loss = dy.esum(batch_loss)
         batch_loss = dy.sum_batches(batch_loss)
 
@@ -32,15 +30,14 @@ class LanguageModelBase:
 
 class LSTMLanguageModel(LanguageModelBase):
     def __init__(self, collection, vocab_size, out_vocab_size, input_embedding_dim=128, \
-            lstm_layers=3, lstm_hidden_dim=256, input_dropout=0.5, recurrent_dropout=0.5):
+            lstm_layers=2, lstm_hidden_dim=256, input_dropout=0.5, recurrent_dropout=0.):
         self.collection = collection
         self.params = {}
 
         self.lstm = dy.VanillaLSTMBuilder(lstm_layers, input_embedding_dim, lstm_hidden_dim, collection)
 
         self.params['W_emb'] = collection.add_lookup_parameters((vocab_size, input_embedding_dim))
-        self.params['R'] = collection.add_parameters((out_vocab_size, lstm_hidden_dim)) 
-        self.params['b'] = collection.add_parameters((out_vocab_size,)) 
+        self.softmax = dy.StandardSoftmaxBuilder(lstm_hidden_dim, out_vocab_size, collection)
 
         self.layers = lstm_layers
         self.input_dropout = input_dropout
@@ -48,58 +45,46 @@ class LSTMLanguageModel(LanguageModelBase):
 
     def get_params(self):
         W_emb = self.params['W_emb']
-        R = dy.parameter(self.params['R'])
-        b = dy.parameter(self.params['b'])
-        return W_emb, R, b
+        softmax = self.softmax
+        return W_emb, softmax
 
     def backward_batch(self, X_batch, y_batch, X_masks):
-        W_emb, R, b = self.get_params()
+        W_emb, softmax = self.get_params()
         X = [ dy.lookup_batch(W_emb, tok_batch) for tok_batch in X_batch ]
 
         self.lstm.set_dropouts(self.input_dropout, self.recurrent_dropout)
         s0 = self.lstm.initial_state()
         states = s0.transduce(X)
-        decoding = [ dy.affine_transform([b, R, h_i]) for h_i in states ]
 
-        return decoding
+        return states
 
     def forward_batch(self, X_batch, maxlen, X_masks, eos):
-        W_emb, R, b = self.get_params()
+        W_emb, softmax = self.get_params()
         X = [ dy.lookup_batch(W_emb, tok_batch) for tok_batch in X_batch ]
 
         self.lstm.set_dropouts(0, 0)
         s0 = self.lstm.initial_state()
         states = s0.transduce(X)
-        decoding = [ dy.affine_transform([b, R, h_i]) for h_i in states ]
 
-        return decoding
+        return states
 
-    def sample_batch(self, batch_size, maxlen, eos):
-        W_emb, R, b = self.get_params()
+    def pickneglogsoftmax_batch(self, x, y):
+        W_emb, softmax = self.get_params()
+        return softmax.neg_log_softmax_batch(x, y)
+
+    def sample_one(self, maxlen, eos):
+        W_emb, softmax = self.get_params()
         self.lstm.set_dropouts(0, 0)
         s0 = self.lstm.initial_state()
-        
-        eoses = dy.lookup_batch(W_emb, [ eos for i in xrange(0, batch_size) ])
-        state = s0.add_input(eoses)
+        state = s0.add_input(W_emb[eos])
 
         samples = []
-        decoding = []
         for i in range(0, maxlen):
             #probability dist
             h_i = state.h()[-1]
-            decoding.append(dy.affine_transform([b, R, h_i]))
+            choice = softmax.sample(h_i)
 
-            probs = dy.softmax(decoding[-1])
-            dim = probs.dim()
-            flatten = dy.reshape(probs, (dim[0][0], dim[1]), batch_size=1)
-            
-            beam = []
-            cdfs = np.cumsum(flatten.value().transpose(), axis=0)
-            for cdf in cdfs:
-                choice = sum(cdf < np.random.rand()) - 1
-                beam.append(choice)
-
-            state = state.add_input(dy.lookup_batch(W_emb, beam))
-            samples.append(beam)
+            samples.append(choice)
+            state = state.add_input(W_emb[choice])
 
         return samples

test.py
@@ -50,7 +50,7 @@ def accuracy(X_valid, y_valid, X_valid_masks, y_valid_masks, X_valid_raw, y_vali
     return val_loss, seq_accuracy, metrics
 
 def perplexity(X_valid, y_valid, X_valid_masks, y_valid_masks, X_valid_raw, y_valid_raw, \
-        dy, lm, out_vocab, run='/runs/baseline', valid_fn='validation'):
+        dy, lm, out_vocab, validate_samples=True, run='/runs/baseline', valid_fn='validation'):
     val_loss = 0.
     l = 0.
 
@@ -73,11 +73,15 @@ def perplexity(X_valid, y_valid, X_valid_masks, y_valid_masks, X_valid_raw, y_va
     perplexity = np.power(2, l)
 
     #validate some samples from lm
-    #samples = lm.sample_batch(32, 20, X_batch[0][0])
-    #samples = lm.to_sequence_batch(samples, out_vocab)
-    #for sample in samples:
-    #    validation.write('%s\n' % ' '.join(sample))
-    #validation.close()
+    if validate_samples:
+        samples = []
+        for i in range(0, 32):
+            sample = lm.sample_one(20, X_batch[0][0])
+            samples.append(lm.to_sequence(sample, out_vocab))
+
+        for sample in samples:
+            validation.write('%s\n' % ' '.join(sample))
+        validation.close()
 
     metrics = [ ('Perplexity: %f.', perplexity) ]
     return val_loss, -perplexity, metrics