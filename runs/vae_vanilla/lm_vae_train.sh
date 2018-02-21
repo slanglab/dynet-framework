8f4b8a5bf77082fb437ede7f7cd247d2a4552189
Johnny Wei <johntzwei@yahoo.com>
2018-02-20 23:11:16 -0500

    this commit is dedicated to june for the vae models

---
+++ runs/vae_vanilla/lm_vae_train.sh
@@ -0,0 +1,22 @@
+python $SEQ2SEQROOT/main.py \
+    --run runs/vae_vanilla/ \
+    --model VanillaVAE \
+    --train data/lm/train \
+    --dev data/lm/valid \
+    --in_vocab data/lm/vocab \
+    --out_vocab data/lm/vocab \
+    --format lm \
+    --val_metric perplexity \
+    --cutoff 0 \
+    --mem 11264 \
+    --gpus 1 \
+    --imports vae \
+    --checkpoint vae_bowman.model \
+    --epochs 100 \
+    --trainer sgd \
+    --lr 1 \
+    --lr_decay 0.85 \
+    --patience 3 \
+    --monitor none \
+    --batch_size 16 \
+    --val_batch_size 64
